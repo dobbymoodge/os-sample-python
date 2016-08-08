45160fc6f7b6f45a8c338827804deb6a36fbbb4d
dobbymoodge <jolamb@redhat.com>
2016-08-08 11:16:40 -0400

    x

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from flask import Flask
 application = Flask(__name__)
 
-@application.route("/")
+@application.route("/<string:arg1>")
 def hello(arg1):
     if arg1:
         return "Hello my main daimie! %s"%arg1
